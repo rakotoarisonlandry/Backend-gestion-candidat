437cafdfdd9e69c8df3275986fb837984ebdc3a6
rakotoarison landry <rakotorisonlandry@gmail.com>
2026-04-07 10:26:58 +0200

    feat: import dotenv and configure environment variables in auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,4 +1,6 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const authMiddleware = (req: any, res: any, next: any) => {
   const header = req.headers.authorization;
