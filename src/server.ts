2c8a61bdfce2b94658581938e77f4ec8e33cfa60
rakotoarison landry <rakotorisonlandry@gmail.com>
2026-04-07 16:20:20 +0200

    feat: update CORS configuration to allow requests from frontend deployment

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173" , "https://frontend-gestion-candidat.onrender.com"],
   credentials: true,
 }));
 
